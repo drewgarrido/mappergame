73113630ce1a5be709c5568c0e558bf1c6186df2
drewgarrido <drewgarrido@gmail.com>
2016-01-18 19:53:02 -0500

    Fixed .html encoding error. Added acceleration and friction to spider movement.

--- mapper.js
+++ mapper.js
@@ -37,80 +37,108 @@ function main()
     requestAnimationFrame(main);
 }
 
-var Spider = function(ctx_pa)
+var Spider = function(ctx_pa, width_pa, height_pa)
 {
     this.x = 0;
     this.y = 0;
-    this.move_x = 0;
-    this.move_y = 0;
+    this.cmd_x = 0;
+    this.cmd_y = 0;
+    this.vel_x = 0;
+    this.vel_y = 0;
+    this.max_vel_sq = 9;
+    this.acc = 0.05;
+    this.friction = 0.2;
     this.icon = loadImage("spider.png");
+    this.max_x = width_pa - this.icon.width;
+    this.max_y = height_pa - this.icon.height;
     var ctx = ctx_pa;
 
     this.render = function()
     {
-        this.x += this.move_x;
+        this.vel_x += this.acc * this.cmd_x;
+        this.vel_y += this.acc * this.cmd_y;
+        if (!this.cmd_x)
+        {
+            if (this.vel_x > this.friction)
+            {
+                this.vel_x -= this.friction;
+            }
+            else if (this.vel_x < -this.friction)
+            {
+                this.vel_x += this.friction;
+            }
+            else
+            {
+                this.vel_x = 0;
+            }
+        }
+        if (!this.cmd_y)
+        {
+            if (this.vel_y > this.friction)
+            {
+                this.vel_y -= this.friction;
+            }
+            else if (this.vel_y < -this.friction)
+            {
+                this.vel_y += this.friction;
+            }
+            else
+            {
+                this.vel_y = 0;
+            }
+        }
 
-        if (this.x < 0)
+        var speed = Math.pow(this.vel_x, 2) + Math.pow(this.vel_y, 2);
+        if (speed > this.max_vel_sq)
         {
-            this.x = 0;
+            var scale = Math.sqrt(this.max_vel_sq/speed);
+            this.vel_x *= scale;
+            this.vel_y *= scale;
         }
-        else if (this.x > 600)
-        {
-            this.x = 600;
-        }
-
-        this.y += this.move_y;
-
-        if (this.y < 0)
-        {
-            this.y = 0;
-        }
-        else if (this.y > 450)
-        {
-            this.y = 450;
-        }
+        this.x = Math.min(this.max_x, Math.max(0, this.x + this.vel_x));
+        this.y = Math.min(this.max_y, Math.max(0, this.y + this.vel_y));
 
         ctx.drawImage(this.icon, this.x, this.y);
     };
 
     this.upArrowDown = function()
     {
-        this.move_y = -1;
+        this.cmd_y = -1;
     };
 
     this.downArrowDown = function()
     {
-        this.move_y = 1;
+        this.cmd_y = 1;
     };
 
     this.leftArrowDown = function()
     {
-        this.move_x = -1;
+        this.cmd_x = -1;
     };
 
     this.rightArrowDown = function()
     {
-        this.move_x = 1;
+        this.cmd_x = 1;
     };
 
     this.upArrowUp = function()
     {
-        this.move_y = 0;
+        this.cmd_y = 0;
     };
 
     this.downArrowUp = function()
     {
-        this.move_y = 0;
+        this.cmd_y = 0;
     };
 
     this.leftArrowUp = function()
     {
-        this.move_x = 0;
+        this.cmd_x = 0;
     };
 
     this.rightArrowUp = function()
     {
-        this.move_x = 0;
+        this.cmd_x = 0;
     };
 
 }
@@ -159,8 +187,6 @@ function checkKeyUp(e)
     }
 }
 
-
-
 function loadImage(src)
 {
     var img1 = false;
@@ -171,8 +197,6 @@ function loadImage(src)
     }
     return img1;
 }
-
-
 
 function initialize()
 {
@@ -185,7 +209,7 @@ function initialize()
     mcc = mc.getContext("2d");
     bmcc = bmc.getContext("2d");
 
-    player = new Spider(bmcc);
+    player = new Spider(bmcc, bmc.width, bmc.height);
 
     document.onkeydown = checkKeyDown;
     document.onkeyup = checkKeyUp;
